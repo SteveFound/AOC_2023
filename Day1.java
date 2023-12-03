45eedb19e6089334de955a3cf924ed261e68bb8e
Steve Found <stephen@foundfamily.co.uk>
2023-12-03 22:04:11 +0000

    Day 3

--- Day1.java
+++ Day1.java
@@ -3,8 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,50 +29,13 @@ public class Day1 {
     }
 
     /**
-     * Find the first and last digits in a line and return them as a 2 digit integer.
-     * @param line string containing the digit(s)
-     * @return a 2 digit integer.
+     * Find the values of the first and last elements of a text digit representation in a string and
+     * return it as a 2 digit integer.
+     * @param line file contents
+     * @param digits map of text representations of digits and their numeric values.
+     * @return integer created by the first digit * 10 + the last digit. 0 if there are no digits.
      */
-    private int processDigits(String line){
-        int first = 0, last = 0;
-
-        // Find the first digit
-        Matcher matcher = Pattern.compile("\\d").matcher(line);
-        if ( matcher.find() ) {
-            first = Integer.parseInt(matcher.group());
-            last = first;
-        }
-
-        // Find the last digit
-        matcher = Pattern.compile("(\\d)(?!.*\\d)").matcher(line);
-        if(matcher.find()) {
-            last = Integer.parseInt(matcher.group());
-        }
-
-        return (first * 10) + last;
-    }
-
-    /**
-     * Looks for the first and last word representations of digits or digits and returns them as a 2
-     * digit number. The problem with the method I have used is that each line has to be checked 40 times.
-     * 20 times for the first digit and 20 times for the last digit... which is pretty awful.
-     *
-     * @param line the line which may contain the digits
-     * @return a 2 digit number as an integer
-     */
-    private int processTextAndDigits(String line) {
-        Map<String, Integer> digits = Stream.of(new Object[][] {
-                {"zero", 0},  {"0", 0},
-                {"one", 1},   {"1", 1},
-                {"two", 2},   {"2", 2},
-                {"three", 3}, {"3", 3},
-                {"four", 4},  {"4", 4},
-                {"five", 5},  {"5", 5},
-                {"six", 6},   {"6", 6},
-                {"seven", 7}, {"7", 7},
-                {"eight", 8}, {"8", 8},
-                {"nine", 9},  {"9", 9}
-        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+    private int firstAndLast(String line, Map<String, Integer> digits) {
         int first = 0, last = 0;
         int firstIdx = 20000;
         int lastIdx = -1;
@@ -98,12 +59,32 @@ public class Day1 {
     }
 
     public void run() {
+        Map<String, Integer> digits = Stream.of(new Object[][] {
+                {"0", 0}, {"1", 1}, {"2", 2}, {"3", 3}, {"4", 4},
+                {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}
+        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+
+        Map<String, Integer> wordsAndDigits = Stream.of(new Object[][] {
+                {"zero", 0},  {"0", 0},
+                {"one", 1},   {"1", 1},
+                {"two", 2},   {"2", 2},
+                {"three", 3}, {"3", 3},
+                {"four", 4},  {"4", 4},
+                {"five", 5},  {"5", 5},
+                {"six", 6},   {"6", 6},
+                {"seven", 7}, {"7", 7},
+                {"eight", 8}, {"8", 8},
+                {"nine", 9},  {"9", 9}
+        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
+
         ArrayList<String> fileContent = readFile();
         int sum = 0;
         int sum2 = 0;
         for( String line : fileContent ){
-            sum += processDigits(line);
-            sum2 += processTextAndDigits(line);
+            // Check for digits only
+            sum += firstAndLast(line, digits);
+            // Check for words or digits
+            sum2 += firstAndLast(line, wordsAndDigits);
         }
         System.out.println("Digits Sum : " + sum);
         System.out.println("Text and Digits sum : " + sum2);
