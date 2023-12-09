340b274dd6cd7d47f11be4df66e0d5ae42b826c3
Steve Found <stephen@foundfamily.co.uk>
2023-12-09 17:18:50 +0000

    Day 9

--- Runner.java
+++ Runner.java
@@ -6,11 +6,12 @@ import day5.Day5;
 import day6.Day6;
 import day7.Day7;
 import day8.Day8;
+import day9.Day9;
 import util.Day;
 
 public class Runner {
     public static void main(String[] args) {
-        Day day = new Day8();
+        Day day = new Day9();
         day.run();
     }
 }
---
+++ day9/Day9.java
@@ -0,0 +1,74 @@
+package day9;
+
+import util.Day;
+import util.FileLoader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day9 implements Day {
+
+    /**
+     * Create a list of numbers that are the differences between the values in a given sequence
+     * @param sequence current sequence
+     * @return list of differences
+     */
+    private List<Integer> getDifferences(List<Integer> sequence) {
+        List<Integer> differences = new ArrayList<>();
+        for (int i = 0; i < sequence.size() - 1; i++) {
+            differences.add(sequence.get(i + 1) - sequence.get(i));
+        }
+        return differences;
+    }
+
+    /**
+     * Recursively calculate the next number in a sequence
+     * @param sequence the sequence to extend
+     * @return the next value in the sequence
+     */
+    private int getNextNumber(List<Integer> sequence) {
+        List<Integer> differences = getDifferences(sequence);
+        if (differences.stream().anyMatch(x -> x != 0)) {
+            return sequence.get(sequence.size() - 1) + getNextNumber(differences);
+        } else {
+            return sequence.get(sequence.size() - 1);
+        }
+    }
+
+    /**
+     * Get the previous number to a sequence. To do this we simply reverse the sequence and find the next number.
+     * @param sequence the sequence to pre-extend
+     * @return the value before the start of the sequence
+     */
+    private int getPreviousNumber(List<Integer> sequence) {
+        ArrayList<Integer> numbers = new ArrayList<>(sequence);
+        Collections.reverse(numbers);
+        return getNextNumber(numbers);
+    }
+
+    private long part1(List<List<Integer>> fileContent ){
+        long sum = 0;
+        for( List<Integer> line : fileContent ){
+            sum += getNextNumber(line);
+        }
+        return sum;
+    }
+
+    private long part2(List<List<Integer>> fileContent ) {
+        long sum = 0;
+        for( List<Integer> line : fileContent ){
+            sum += getPreviousNumber(line);
+        }
+        return sum;
+    }
+
+    @Override
+    public void run() {
+        List<List<Integer>> fileContent =
+                FileLoader.readFileAsIntegers("input/day9.txt", "\\s+");
+
+        System.out.println("Part1: " + part1(fileContent));
+        System.out.println("Part2: " + part2(fileContent));
+    }
+}
